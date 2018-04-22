e8d0f45ed42f0a4d19d3bdf8fe80e4c76efea856
Hoang Duc Thang <thanghoang07@gmail.com>
2018-04-22 21:01:51 +0700

    phần carousel bị lỗi

--- SmartHotel/SmartHotel/Views/Templates/MenuItemTemplate.xaml.cs
+++ SmartHotel/SmartHotel/Views/Templates/MenuItemTemplate.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Windows.Input;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -16,5 +16,14 @@ namespace SmartHotel.Views.Templates
 		{
 			InitializeComponent ();
 		}
-	}
+
+        public static readonly BindableProperty TapCommandProperty =
+           BindableProperty.Create("TapCommand", typeof(ICommand), typeof(NotificationItemTemplate));
+
+        public ICommand TapCommand
+        {
+            get { return (ICommand)GetValue(TapCommandProperty); }
+            set { SetValue(TapCommandProperty, value); }
+        }
+    }
 }
