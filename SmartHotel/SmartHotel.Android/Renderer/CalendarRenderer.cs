6ba03021021c380e23471c63221b39d0eb20543a
Hoang Duc Thang <thanghoang07@gmail.com>
2018-05-04 21:08:55 +0700

    Renderer

---
+++ SmartHotel/SmartHotel.Android/Renderer/CalendarRenderer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Xamarin.Forms.Platform.Android;
+using SmartHotel.Controls;
+using SmartHotel.Droid.Renderers;
+using Xamarin.Forms;
+
+[assembly: ExportRenderer(typeof(Calendar), typeof(CalendarRenderer))]
+namespace SmartHotel.Droid.Renderers
+{
+    public class CalendarRenderer : ViewRenderer<Calendar, CalendarView>
+    {
+        public CalendarRenderer(Context context) : base(context)
+        {
+
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Calendar> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.NewElement != null)
+            {
+                SetNativeControl(new CalendarView(Context));
+            }
+        }
+    }
+}
---
+++ SmartHotel/SmartHotel.Android/Renderer/ExtendedEntryRenderer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using SmartHotel.Controls;
+using SmartHotel.Droid.Renderer;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(ExtendedEntry), typeof(ExtendedEntryRenderer))]
+namespace SmartHotel.Droid.Renderer
+{
+   public class ExtendedEntryRenderer :EntryRenderer
+    {
+        public ExtendedEntryRenderer(Context context) : base(context)
+        {
+
+        }
+
+        // vua renderer
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            if(e.NewElement != null)
+            {
+                           
+            }
+        }
+
+        // update lai do ly do nao do dung OnElementPropertyChanged
+    }
+}
